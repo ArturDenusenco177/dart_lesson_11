6b9a030fff79b0fc20626581dc598996616c0386
Artur D <bleackbeard177@gmail.com>
2025-07-11 22:25:24 +0300

    feat(DART-000): initial file

---
+++ bin/dart_lesson_11.dart
@@ -0,0 +1,5 @@
+import 'package:dart_lesson_11/dart_lesson_11.dart' as dart_lesson_11;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_lesson_11.calculate()}!');
+}
---
+++ test/dart_lesson_11_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_lesson_11/dart_lesson_11.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
