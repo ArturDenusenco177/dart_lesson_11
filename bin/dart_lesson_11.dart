3bac596d8d692b51ceb1aed7af6eed3c61f2c405
Artur D <bleackbeard177@gmail.com>
2025-07-12 18:20:49 +0300

    feat(DART-017): stream periodic

--- bin/dart_lesson_11.dart
+++ bin/dart_lesson_11.dart
@@ -3,63 +3,61 @@ import 'package:dart_lesson_11/streams/hw_streams.dart';
 
 // Варіант 1: Використання async/await
 Future<void> main() async {
-  final stopwatch = Stopwatch();
-  stopwatch.start();
+  // final stopwatch = Stopwatch();
+  // stopwatch.start();
 
-  print('------------------- Task 1 -------------------');
-  //1
-  final futureName = await fetchName();
-  print('Моє ім\'я: ${futureName}');
+  // print('------------------- Task 1 -------------------');
+  // //1
+  // final futureName = await fetchName();
+  // print('Моє ім\'я: ${futureName}');
 
-  //2
-  print('------------------- Task 2 -------------------');
-  final futureAge = await fetchAge();
-  String year;
-  int lastDigit = futureAge % 10;
-  int lastTwoDigits = futureAge % 100;
+  // //2
+  // print('------------------- Task 2 -------------------');
+  // final futureAge = await fetchAge();
+  // String year;
+  // int lastDigit = futureAge % 10;
+  // int lastTwoDigits = futureAge % 100;
 
-  if (lastDigit == 1 && lastTwoDigits != 11) {
-    year = 'рік';
-  } else if ([2, 3, 4].contains(lastDigit) &&
-      !(lastTwoDigits >= 12 && lastTwoDigits <= 14)) {
-    year = 'роки';
-  } else {
-    year = 'років';
-  }
+  // if (lastDigit == 1 && lastTwoDigits != 11) {
+  //   year = 'рік';
+  // } else if ([2, 3, 4].contains(lastDigit) &&
+  //     !(lastTwoDigits >= 12 && lastTwoDigits <= 14)) {
+  //   year = 'роки';
+  // } else {
+  //   year = 'років';
+  // }
 
-  print('Мені $futureAge $year.');
+  // print('Мені $futureAge $year.');
 
-  //3
-  print('------------------- Task 3 -------------------');
-  stopwatch.stop();
-  print('Час виконання: ${stopwatch.elapsedMicroseconds} мікросекунд');
-  print('Послідовне виконання завершено');
+  // //3
+  // print('------------------- Task 3 -------------------');
+  // stopwatch.stop();
+  // print('Час виконання: ${stopwatch.elapsedMicroseconds} мікросекунд');
+  // print('Послідовне виконання завершено');
 
-  //4
-  print('------------------- Task 4 -------------------');
-  stopwatch.reset();
-  stopwatch.start();
-  final listFuture = await Future.wait([fetchName(), fetchAge()]);
+  // //4
+  // print('------------------- Task 4 -------------------');
+  // stopwatch.reset();
+  // stopwatch.start();
+  // final listFuture = await Future.wait([fetchName(), fetchAge()]);
 
-  print('Моє ім\'я: ${listFuture[0]}');
-  print('Мені ${listFuture[1]} $year.');
-  stopwatch.stop();
-  print('Час виконання: ${stopwatch.elapsedMicroseconds} мікросекунд');
+  // print('Моє ім\'я: ${listFuture[0]}');
+  // print('Мені ${listFuture[1]} $year.');
+  // stopwatch.stop();
+  // print('Час виконання: ${stopwatch.elapsedMicroseconds} мікросекунд');
 
-  //5
-  print('------------------- Task 5 -------------------');
-  final taim = await delayedCountdown(5);
-  print(taim);
+  // //5
+  // print('------------------- Task 5 -------------------');
+  // final taim = await delayedCountdown(5);
+  // print(taim);
 
-  //6
-  print('------------------- Task 6 -------------------');
-  await streamSix();
+  // //6
+  // print('------------------- Task 6 -------------------');
+  // await streamSix();
 
   //7
   print('------------------- Task 7 -------------------');
-  
-
-
+  await streamSeven();
 }
 
 // Варіант 2: Використання then
--- lib/streams/hw_streams.dart
+++ lib/streams/hw_streams.dart
@@ -3,9 +3,19 @@ Future<void> streamSix() async {
   Stream<int> stream = Stream.fromIterable([1, 2, 3, 4, 5]);
   await for (int value in stream) {
     print('Отримано значення з потоку: $value');
-  };
-  stream.listen((task) => {print('Отримано значення з потоку: $task')});
+  }
+  ;
+  //stream.listen((task) => {print('Отримано значення з потоку: $task')});
 }
 
 //7
+Future<void> streamSeven() async {
+  Stream<int> stream = Stream.periodic(
+    Duration(seconds: 1),
+    (count) => count + 1,
+  ).take(10);
 
+  await for (int value in stream) {
+    print('$value...');
+  }
+}
