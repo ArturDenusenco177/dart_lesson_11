54d162fc9efcd2569398fc48f773020d5256bf7e
Artur D <bleackbeard177@gmail.com>
2025-07-12 18:40:49 +0300

    feat(DART-018): stream controller

--- lib/streams/hw_streams.dart
+++ lib/streams/hw_streams.dart
@@ -1,4 +1,6 @@
 //6
+import 'dart:async';
+
 Future<void> streamSix() async {
   Stream<int> stream = Stream.fromIterable([1, 2, 3, 4, 5]);
   await for (int value in stream) {
@@ -18,4 +20,20 @@ Future<void> streamSeven() async {
   await for (int value in stream) {
     print('$value...');
   }
+}
+
+//8
+void streamEight() {
+  final controller = StreamController<String>();
+  final stream = controller.stream;
+  final sink = controller.sink;
+
+  sink.add('Hello');
+  sink.add('World');
+  sink.add('Dart');
+  sink.close();
+
+  stream.listen((data) {
+    print("Дані $data");
+  });
 }
